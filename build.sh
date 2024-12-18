4fb02788e7964c2f333d66052fce8c70b0da890b
JUAN CAMILO BARRAGAN VILLALOBOS <juancamilo2041@gmail.com>
2024-12-18 15:58:41 -0500

    1.2

--- build.sh
+++ build.sh
@@ -3,14 +3,17 @@
 # Build the project
 set -e  # Exit on error
 
+# Actualizar pip
+pip install --upgrade pip
+
 # Instalar dependencias
 pip install -r requirements.txt
 
-pip install --upgrade pip
+# Eliminar archivos estáticos antiguos
+rm -rf /opt/render/project/src/static
 
 # Recopilar archivos estáticos
 python manage.py collectstatic --noinput
 
-# Aplicar migraciones
+# Aplicar migraciones a la base de datos
 python manage.py migrate
-
