c81113d3c972cb86909741f3409049a8a6d8eb8c
JUAN CAMILO BARRAGAN VILLALOBOS <juancamilo2041@gmail.com>
2024-12-18 15:40:05 -0500

    1.1

--- build.sh
+++ build.sh
@@ -6,8 +6,11 @@ set -e  # Exit on error
 # Instalar dependencias
 pip install -r requirements.txt
 
+pip install --upgrade pip
+
 # Recopilar archivos estáticos
 python manage.py collectstatic --noinput
 
 # Aplicar migraciones
 python manage.py migrate
+
